b43ec2f109b205752764b87d602a0e90d3bebce8
Jimmy Lu <jimmy.lu.2011@gmail.com>
2024-01-14 22:10:56 -0800

    add server-side registration and authentication

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,162 @@
 #[macro_use]
 extern crate rocket;
 
+use bcrypt::{hash, verify};
+use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use rocket::http::Status;
+use rocket::request::{self, FromRequest, Request};
 use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket::State;
-use rocket_cors::{AllowedOrigins, CorsOptions};
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
+use rusqlite::{params, Connection};
 use std::fs::{File, OpenOptions};
 use std::io::{self, Read, Write};
 use std::net::IpAddr;
 use std::str::FromStr;
 use std::sync::Mutex;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
+fn create_db() -> rusqlite::Result<()> {
+    let conn = Connection::open("chat.db")?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+                  id INTEGER PRIMARY KEY,
+                  username TEXT NOT NULL UNIQUE,
+                  password_hash TEXT NOT NULL
+         )",
+        [],
+    )?;
+    Ok(())
+}
+
+/*
+ * User authentication
+ */
+#[derive(Serialize, Deserialize)]
+struct User {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct JwtClaim {
+    sub: String,
+    exp: usize,
+}
+
+struct AuthenticatedUser {
+    username: String,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for AuthenticatedUser {
+    type Error = ();
+
+    async fn from_request(
+        req: &'r Request<'_>,
+    ) -> request::Outcome<AuthenticatedUser, Self::Error> {
+        let authorization_header = req.headers().get_one("Authorization");
+
+        let token = match authorization_header {
+            Some(header) => {
+                // Split the header to separate "Bearer" from the actual token
+                let parts: Vec<&str> = header.split_whitespace().collect();
+                if parts.len() == 2 && parts[0] == "Bearer" {
+                    Some(parts[1])
+                } else {
+                    None
+                }
+            }
+            None => None,
+        };
+        match token {
+            Some(token) => {
+                match decode::<JwtClaim>(
+                    &token,
+                    &DecodingKey::from_secret("secret_key".as_ref()),
+                    &Validation::default(),
+                ) {
+                    Ok(c) => request::Outcome::Success(AuthenticatedUser {
+                        username: c.claims.sub,
+                    }),
+                    Err(_) => request::Outcome::Error((Status::Unauthorized, ())),
+                }
+            }
+            None => request::Outcome::Error((Status::Unauthorized, ())),
+        }
+    }
+}
+
+fn register_user(username: String, password: String) -> Result<(), String> {
+    let password_hash = hash(password, bcrypt::DEFAULT_COST).map_err(|e| e.to_string())?;
+    let conn = Connection::open("chat.db").map_err(|e| e.to_string())?;
+
+    conn.execute(
+        "INSERT INTO users (username, password_hash) VALUES (?1, ?2)",
+        params![username, password_hash],
+    )
+    .map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
+fn authenticate_user(username: String, password: String) -> Result<String, String> {
+    let conn = Connection::open("chat.db").map_err(|e| e.to_string())?;
+    let mut stmt = conn
+        .prepare("SELECT password_hash FROM users WHERE username = ?1")
+        .map_err(|e| e.to_string())?;
+    let password_hash: String = stmt
+        .query_row(params![username], |row| row.get(0))
+        .map_err(|e| e.to_string())?;
+
+    if verify(password, &password_hash).map_err(|e| e.to_string())? {
+        // Get the current time as a Unix timestamp
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        let expiration_time = now
+            .checked_add(Duration::from_secs(3600)) // Add 3600 seconds (1 hour) to the current time
+            .expect("Invalid timestamp")
+            .as_secs();
+        let expiration = expiration_time;
+
+        let claims = JwtClaim {
+            sub: username,
+            exp: expiration as usize,
+        };
+
+        let token = encode(
+            &Header::default(),
+            &claims,
+            &EncodingKey::from_secret("secret_key".as_ref()),
+        )
+        .map_err(|e| e.to_string())?;
+        println!("Returning token: {:?}", token);
+        Ok(token)
+    } else {
+        Err("Invalid username or password".to_string())
+    }
+}
+
+#[post("/register", format = "json", data = "<user_data>")]
+async fn register(user_data: Json<User>) -> Json<Result<(), String>> {
+    Json(register_user(
+        user_data.username.clone(),
+        user_data.password.clone(),
+    ))
+}
+
+#[post("/login", format = "json", data = "<login_data>")]
+async fn login(login_data: Json<User>) -> Json<Result<String, String>> {
+    Json(authenticate_user(
+        login_data.username.clone(),
+        login_data.password.clone(),
+    ))
+}
+
+/*
+ * Save and load messages
+ */
 const DATA_FILE: &str = "chat_data.json";
 
 fn load_messages() -> io::Result<Vec<Message>> {
@@ -36,6 +183,9 @@ fn save_messages(messages: &Vec<Message>) -> io::Result<()> {
     Ok(())
 }
 
+/*
+ * Receiving messages and sending messages
+ */
 #[derive(Serialize, Deserialize, Clone)]
 struct Message {
     username: String,
@@ -64,7 +214,7 @@ struct GetMessage {
 }
 
 #[post("/message", format = "json", data = "<message>")]
-fn post_message(message: Json<PostMessage>, state: &State<ChatState>) {
+fn post_message(user: AuthenticatedUser, message: Json<PostMessage>, state: &State<ChatState>) {
     let mut messages = state.messages.lock().unwrap();
     messages.push(Message {
         username: message.username.clone(),
@@ -75,7 +225,8 @@ fn post_message(message: Json<PostMessage>, state: &State<ChatState>) {
 }
 
 #[get("/messages")]
-fn get_messages(state: &State<ChatState>) -> Json<Vec<GetMessage>> {
+// fn get_messages(state: &State<ChatState>) -> Json<Vec<GetMessage>> {
+fn get_messages(user: AuthenticatedUser, state: &State<ChatState>) -> Json<Vec<GetMessage>> {
     let messages = state.messages.lock().unwrap();
     Json(
         messages
@@ -89,11 +240,23 @@ fn get_messages(state: &State<ChatState>) -> Json<Vec<GetMessage>> {
     )
 }
 
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, Rocket Chat Server!"
+}
+
 #[launch]
 fn rocket() -> _ {
+    create_db().expect("Failed to create database");
+
     // This is only needed for local testing using browser for react-native frontend
     let cors = CorsOptions::default()
         .allowed_origins(AllowedOrigins::some_exact(&["http://localhost:19006"]))
+        .allowed_headers(AllowedHeaders::some(&[
+            "Authorization",
+            "Content-Type",
+            // Add other headers as needed
+        ]))
         .to_cors()
         .unwrap();
     let chat_state = ChatState {
@@ -106,10 +269,8 @@ fn rocket() -> _ {
         })
         .attach(cors)
         .manage(chat_state)
-        .mount("/", routes![index, post_message, get_messages])
-}
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, Rocket Chat Server!"
+        .mount(
+            "/",
+            routes![index, register, login, post_message, get_messages],
+        )
 }
